21db495d667eb530a3416631efc35aa05c0f5179
Eric Bittleman <eric.bittleman@gmail.com>
2015-05-31 16:31:29 -0400

    added a test

--- test/MonologZf2Test/MonologTest.php
+++ test/MonologZf2Test/MonologTest.php
@@ -2,13 +2,32 @@
 namespace MonologZfTest;
 
 use MailgunZf2Test\UnitTest;
+use Monolog\Handler\TestHandler;
 
 class MonologTest extends UnitTest
 {
-    public function testLog()
+    public function testLogGet()
     {
         $log = $this->sm->get('Monolog\Log');
 
         $this->assertInstanceOf('Monolog\Logger', $log);
+    }
+
+
+    public function testLogWrite()
+    {
+        $handler = new TestHandler;
+        $log = $this->sm->get('Monolog\Log');
+
+        $log->pushHandler($handler);
+
+        $log->debug('logline');
+
+        $this->assertTrue($handler->hasDebugRecords());
+
+        $records = $handler->getRecords();
+        $record = $records[0];
+
+        $this->assertEquals('logline', $record['message']);
     }
 }
--- test/config/config.global.php
+++ test/config/config.global.php
@@ -7,22 +7,7 @@ return array(
         'defaultLogger' => 'Monolog\Log',
         'logs' => array(
             'Monolog\Log' => array(
-                'handlers' => array(
-                    array(
-                        'handlerClass' => 'Monolog\Handler\StreamHandler',
-                        'args' => array(
-                            __DIR__ . '/../test.log',
-                            Logger::DEBUG
-                        )
-                    ),
-                    array(
-                        'handlerClass' => 'Monolog\Handler\ErrorLogHandler',
-                        'args' => array(
-                            ErrorLogHandler::OPERATING_SYSTEM,
-                            Logger::ERROR
-                        )
-                    )
-                )
+                'handlers' => array()
             ),
             'Monolog\ErrorLog' => array(
                 'handlers' => array(
